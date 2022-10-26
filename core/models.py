1fb0f404545ef08732831c5d7b15e2d0cbdb59f3
athmanziri <issaziri@gmail.com>
2022-10-26 11:58:11 +0300

    [UPDATE] core/models.py

--- core/models.py
+++ core/models.py
@@ -9,8 +9,13 @@ class Record(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     # voice_record = models.FileField(
     #     upload_to="records", storage=RawMediaCloudinaryStorage())
-    voice_record = models.FileField(upload_to="records")
+    word = models.CharField(max_length=50, null=True, blank=True)
+    meaning = models.CharField(max_length=100, null=True, blank=True)
+    meaning_in_english = models.CharField(max_length=100, null=True, blank=True)
+    meaning_in_swahili = models.CharField(max_length=100, null=True, blank=True)
     language = models.CharField(max_length=50, null=True, blank=True)
+    voice_record = models.FileField(upload_to="records", null=True, blank=True)
+    voice_record_use_case = models.FileField(upload_to="use_cases", null=True, blank=True)
 
     class Meta:
         verbose_name = "Record"
